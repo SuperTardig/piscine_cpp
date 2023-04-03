af894d1a17d0376772527f3affdf43b9da11463c
SuperTardig <benjaminperron150@gmail.com>
2023-04-03 12:30:46 -0400

    fixed cpp06

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -11,107 +11,155 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include <cmath>
 #include <limits>
+
 
 using std::cout;
 using std::endl;
 using std::string;
 
-string	rm_spaces(string input){
-	int start = 0, end = 0;
-
-	while (input[start] && isspace(input[start]))
-		start++;
-	while (input[start + end] && !isspace(start + end))
-		end++;
-	return string(input, start, end);
+void	print_string(string output[4]){
+	cout << "char: " << output[0] << endl;
+	cout << "int: " << output[1] << endl;
+	cout << "float: " << output[2] << endl;
+	cout << "double: " << output[3] << endl;
 }
 
-void	print_char(double nb){
-	cout << "char : ";
-	if (nb > UCHAR_MAX || nb < 0 || isnan(nb)){
-		cout << "impossible" << endl;
-		return;
-	}
-	if (!isprint(nb)){
-		cout << "non printable" << endl;
-		return;
-	}
-	cout << "'" << static_cast<char>(nb) << "'" << endl;
-	return;
-}
-
-void	print_int(double nb){
-	cout << "int : ";
-	if (nb > INT_MAX || nb < INT_MIN || isnan(nb)){
-		cout << "impossible" << endl;
-		return;
-	}
-	cout << static_cast<int>(nb) << endl;
-	return;
-}
-
-void	print_float(double nb){
-	cout << "float : ";
-	if (isinf(nb)){
-		if (nb < 0)
-			cout << "-";
-		cout << "inf" << endl;
-		return;
-	}
-	if (isnan(nb)){
-		cout << "NaNf" << endl;
-		return;
-	}
-	if (nb == static_cast<int>(nb))
-		cout << static_cast<float>(nb) << ".0f" << endl;
+void	print_char(double c){
+	if (isprint(c) && c == static_cast<int>(c))
+		cout << "char: '" << static_cast<char>(c) << "'" << endl;
 	else
-		cout << static_cast<float>(nb) << "f" << endl;
-	return;
+		cout << "char: Non displayable" << endl;
 }
 
-void	print_double(double nb){
-	cout << "double : ";
-	if (isinf(nb)){
-		if (nb < 0)
-			cout << "-";
-		cout << "inf" << endl;
-		return;
-	}
-	if (isnan(nb)){
-		cout << "NaN" << endl;
-		return;
-	}
-	if (nb == static_cast<int>(nb))
-		cout << static_cast<double>(nb) << ".0" << endl;
+void	print_int(double i){
+	if (trunc(i) == static_cast<int>(i))
+		cout << "int: " << static_cast<int>(i) << endl;
 	else
-		cout << static_cast<double>(nb) << endl;
-	return;
+		cout << "int: Not an int" << endl;
+}
+
+void	print_float(double f){
+	if (static_cast<int>(f) == (double) f)
+		cout << "float: " << static_cast<float>(f) << ".0f" << endl;
+	else
+		cout << "float: " << static_cast<float>(f) << "f" << endl;
+}
+
+void	print_double(double d){
+	if (static_cast<int>(d) == d)
+		cout << "double: " << d << ".0" << endl;
+	else
+		cout << "double: " << d << endl;
+}
+
+void	print_type(double val){
+	print_char(val);
+	print_int(val);
+	print_float(val);
+	print_double(val);
+}
+
+int	findType(string val){
+	int	dot = 0;
+
+	if (val.size() == 1 && isdigit(val.at(0)) == false)
+		return (1);
+	else if (val.find('.', 0) != val.npos)
+	{
+		if (val.find('f', 0) != val.npos){
+			for (size_t i = 0; i < val.size() - 1; i++){
+				if (val.at(i) == '.')
+					dot++;
+				else if (isdigit(val.at(i)) == false)
+					return -1;
+			}
+			if (dot > 1)
+				return -1;
+			return 4;
+		}
+		else {
+			for (size_t i = 0; i < val.size(); i++){
+				if (val.at(i) == '.')
+					dot++;
+				else if (isdigit(val.at(i)) == false)
+					return -1;
+			}
+			if (dot > 1)
+				return -1;
+			return 3;
+		}
+	}
+	else{
+		for (size_t i = 0; i <val.size() ; i++)
+			if (isdigit(val.at(i)) == false)
+					return -1;
+		if (atoi(val.c_str()) != atol(val.c_str()))
+			return 3;
+		return 2;
+	}
 }
 
 int main(int ac, char **av){
-	string hold;
-	double nb;
+	string output[4] = {"impossible", "impossible", "impossible", "impossible"};
 
-	if (ac == 2){
-		hold = rm_spaces(av[1]);
-		if (hold.length() == 1 && !isnumber(hold[0]))
-			nb = hold[0];
-		else{
-			try{
-				nb = stod(hold);
-			}
-			catch(const std::exception& e){
-				std::cerr << e.what() << '\n';
-				return 1;
-			}
-		}
-		print_char(nb);
-		print_int(nb);
-		print_float(nb);
-		print_double(nb);
+	if (ac == 1 || av[0] == 0){
+		std::cout << "Error: number of arguments" << std::endl;
+		return (0);
 	}
-	else
-		cout << "The number of argument entered is wrong" << endl;
+	string val(av[1]);
+	if (val.empty()){
+		std::cout << "Error: number of arguments" << std::endl;
+		return (0);
+	}
+	if (isnan(atof(val.c_str())) != 0 && (val.size() == 3 || val.size() == 4 && val[3] == 'f')){
+		if (val.size() == 3)
+			cout << "The type is: double" << endl;
+		else
+			cout << "The type is: float" << endl;
+		output[2] = "nanf";
+		output[3] = "nan";
+		print_string(output);
+	}
+	else if (isinf(atof(val.c_str())) != 0 && (val.compare("+inf") == 0 || val.compare("-inf") == 0 || val.compare("+inff") == 0 || val.compare("-inff") == 0)){
+		if (val.size() == 5)
+			cout << "The type is: float" << endl;
+		else
+			cout << "The type is: double" << endl;
+		if (val.at(0) == '-'){
+			output[2] = "-inff";
+			output[3] = "-inf";
+		}
+		if (val.at(0) == '+'){
+			output[2] = "+inff";
+			output[3] = "+inf";
+		}
+		print_string(output);
+	}
+	else{
+		switch(findType(val)){
+			case 1:
+				cout << "The type is: char" << endl;
+				print_type(val.at(0));
+				break;
+			case 2:
+				cout << "The type is: int" << endl;
+				print_type(atoi(val.c_str()));
+				break;
+			case 3:
+				cout << "The type is: double" << endl;
+				print_type(atof(val.c_str()));
+				break;
+			case 4:
+				cout << "The type is: float" << endl;
+				print_type(atof(val.c_str()));
+				break;
+			default:
+				cout << "Error: bad argument" << endl;
+				print_string(output);
+				break;
+		}
+	}
 }
